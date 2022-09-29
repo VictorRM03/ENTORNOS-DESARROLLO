418b30463d04459a5faabe820de44a8757152537
VictorRM03 <90856795+VictorRM03@users.noreply.github.com>
2022-09-29 10:07:06 +0200

    Add files via upload

---
+++ Ejercicio-3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Declarar variables
+float a;
+float b;
+
+float suma;
+float resta;
+float multiplicacion;
+float division;
+float modulo;
+
+// Asignando valores a las variables
+a = 45;
+b = 100;
+
+// Realizando las operaciones
+suma = a + b;
+resta = a - b;
+multiplicacion = a * b;
+division = a / b;
+modulo = a % b;
+
+// Mostramos resultados
+Console.WriteLine("Los resultados son: ");
+Console.WriteLine("Suma: " + suma);
+Console.WriteLine("Resta: " + resta);
+Console.WriteLine("Multiplicación: " + multiplicacion);
+Console.WriteLine("División: " + division);
+Console.WriteLine("Modulo: " + modulo);
