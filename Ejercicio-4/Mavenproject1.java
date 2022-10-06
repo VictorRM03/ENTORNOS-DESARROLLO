b455acc5916dc2de58ed6396a1b0d503ba420438
VictorRM03 <90856795+VictorRM03@users.noreply.github.com>
2022-10-06 09:27:06 +0200

    Add files via upload

---
+++ Ejercicio-4/Mavenproject1.java
@@ -0,0 +1,10 @@
+package com.mycompany.mavenproject1;
+/**
+ * @author Víctor
+ * EJERCICIO 4: Prueba de NetBeans
+ */
+public class Mavenproject1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
